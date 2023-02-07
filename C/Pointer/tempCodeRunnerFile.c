284133f9a9db1ba33cdc590dd27132e32b58afef
BorimirGanchev <99996450+BorimirGanchev@users.noreply.github.com>
2023-02-07 10:35:46 +0200

    pointers

---
+++ C/Pointer/tempCodeRunnerFile.c
@@ -0,0 +1,29 @@
+  int * p = &a;
+//   printf("%p\n", p);
+//   //ukazatel kum a
+
+//   *p = 10;
+//   printf("%d\n", a);
+//   //davame celochislena stojnost na ukazatelq
+
+//   int b = 21;
+//   p = &b; //p now points to b;
+//   printf("%d\n", *p);
+
+//   int * k = &b;//
+//   int * l = k;
+
+//   b = 33;
+//   printf("%d\n", b);
+//   printf("%d\n", *k);
+//   printf("%d\n", *l);
+
+//   k = &a;
+//   a = 5;
+//   printf("%d\n", a);
+//   printf("%d\n", *k);
+//   printf("%d\n", *l);
+
+  
+
+//   return 0;
